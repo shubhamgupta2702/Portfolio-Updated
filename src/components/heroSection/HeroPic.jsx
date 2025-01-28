0b168c0e116093e375c00a0bfb5e98227470f92c
Shubham <shubhamgupta43567@gmail.com>
2025-01-29 03:50:53 +0530

    Portfolio updated

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/hexagon.png"
+        src="/images/hexagon.png"
         alt="Shubham Gupta"
         className="max-h-[600px] w-auto"
       />
